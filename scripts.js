16e5b09dcdec01d3716f1366a0e85a84abab9473
Daniel Teles Ribeiro <dan.ribeiro@hotmail.com>
2023-02-23 01:41:25 -0300

    Implemented semantic HTML and Bootstrap form validation in signup.html.

--- scripts.js
+++ scripts.js
@@ -1,21 +1,22 @@
-// Example starter JavaScript for disabling form submissions if there are invalid fields
-(() => {
+// Bootstrap Form Validation
+
+(function () {
   "use strict";
 
-  // Fetch all the forms we want to apply custom Bootstrap validation styles to
-  const forms = document.querySelectorAll(".needs-validation");
+  var forms = document.querySelectorAll(".needs-validation");
 
-  // Loop over them and prevent submission
-  Array.from(forms).forEach((form) => {
+  Array.prototype.slice.call(forms).forEach(function (form) {
     form.addEventListener(
       "submit",
-      (event) => {
+      function (event) {
         if (!form.checkValidity()) {
-          event.preventDefault();
-          event.stopPropagation();
+          form.classList.add("was-validated");
+        } else {
+          form.classList.remove("was-validated");
+          form.reset();
         }
-
-        form.classList.add("was-validated");
+        event.preventDefault();
+        event.stopPropagation();
       },
       false
     );
